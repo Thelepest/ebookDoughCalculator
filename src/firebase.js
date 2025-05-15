5d92a7d9fb7888d3ebf99967bf633f50e1db0b25
Thelepest <marco.biasone.90@gmail.com>
2025-05-15 18:32:42 +0200

    login with email and password or gmail with logout

--- src/firebase.js
+++ src/firebase.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
+console.log("⚙️  Firebase config:", firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
